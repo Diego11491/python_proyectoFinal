f5a6a28fb40c2824cf80b6f4cf6c989b0622f248
Diego Ventura <diego11491@outlook.com>
2025-06-30 23:45:02 -0500

    Arreglos de css

--- app.py
+++ app.py
@@ -102,4 +102,6 @@ def descargar_pdf():
                      mimetype='application/pdf')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
